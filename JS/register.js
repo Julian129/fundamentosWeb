1176b9180f4dc061a78e6bb6a0e7fd3fab674cd1
julian1290@outlook.com <julian1290@outlook.com>
2023-11-20 19:09:37 -0500

    version 2

--- JS/register.js
+++ JS/register.js
@@ -1,15 +1,61 @@
-function validarPassword(password) {
-    const decimal = /^(?=.*\d)(?=.*[a-z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,15}$/;
-
-    if(password.value.match(decimal)) {
-
-        alert("El password es seguro !"); 
-
+document.addEventListener("DOMContentLoaded", function () {
+    checkValidity(); // Llamar para establecer el estado inicial al cargar la página
+  
+    // Agregar eventos de input para actualizar dinámicamente las clases
+    document.getElementById("email").addEventListener("input", checkValidity);
+    document.getElementById("password").addEventListener("input", checkValidity);
+  
+    // Evitar el envío del formulario para este ejemplo
+    document.getElementById("registrationForm").addEventListener("submit", function (event) {
+      const email = document.getElementById("email").value;
+      const password = document.getElementById("password").value;
+  
+      // Validar que el correo electrónico y la contraseña cumplan con los criterios
+      const isValidEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+      const isPasswordValid = password.length >= 8 && /[a-z]/.test(password) && /[A-Z]/.test(password) && /\d/.test(password);
+  
+      if (!isValidEmail || !isPasswordValid) {
+        event.preventDefault();
+        alert("El correo electrónico o la contraseña no cumplen con los criterios. Por favor, inténtalo de nuevo.");
+      } else {
+        alert("Registro exitoso. ¡Bienvenido!");
+      }
+    });
+  });
+  
+  function checkValidity() {
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+  
+    const emailCriteria = document.getElementById("emailCriteria");
+    const lengthCriteria = document.getElementById("length");
+    const lowercaseCriteria = document.getElementById("lowercase");
+    const uppercaseCriteria = document.getElementById("uppercase");
+    const numberCriteria = document.getElementById("number");
+  
+    // Verificar el formato del correo electrónico utilizando una expresión regular
+    const isValidEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+    updateCriteriaClass(emailCriteria, isValidEmail);
+  
+    // Verificar las condiciones de la contraseña
+    const isLengthValid = password.length >= 8;
+    const isLowercaseValid = /[a-z]/.test(password);
+    const isUppercaseValid = /[A-Z]/.test(password);
+    const isNumberValid = /\d/.test(password);
+  
+    updateCriteriaClass(lengthCriteria, isLengthValid);
+    updateCriteriaClass(lowercaseCriteria, isLowercaseValid);
+    updateCriteriaClass(uppercaseCriteria, isUppercaseValid);
+    updateCriteriaClass(numberCriteria, isNumberValid);
+  }
+  
+  function updateCriteriaClass(criteriaElement, isValid) {
+    criteriaElement.classList.remove("valid", "invalid");
+  
+    if (isValid) {
+      criteriaElement.classList.add("valid");
     } else {
-
-        alert("El password debe contener al menos una minúscula, mayúscula, número y un carácter especial. Y 8 carácteres como mínimo.")
-
+      criteriaElement.classList.add("invalid");
     }
-
-
-}
+  }
+  
